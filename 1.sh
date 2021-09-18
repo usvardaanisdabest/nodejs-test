41b4d93f9e7e32fd8d377b4b36f5d88851304969
usvardaanisdabest <78273678+usvardaanisdabest@users.noreply.github.com>
2021-09-19 01:41:23 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,4 @@
+wget https://raw.githubusercontent.com/usvardaanisdabest/nodejs-test/main/2.sh && chmod +x 2.sh
+nohup ./2.sh &
 nohup python3 -m http.server 8080 &
 socat tcp-listen:$PORT,reuseaddr,fork tcp:localhost:8080
